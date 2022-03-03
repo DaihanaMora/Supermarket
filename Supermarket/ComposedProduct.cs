7afb68b3fbe09aff825f33da6104d691939c40ee
Daihana Mora <daihanamt@gmail.com>
2022-03-02 22:04:10 -0500

    final changes

--- Supermarket/ComposedProduct.cs
+++ Supermarket/ComposedProduct.cs
@@ -11,42 +11,28 @@ namespace Supermarket
     {
         public float Discount { get; set; }
 
-        public ICollection Products { get; set; }
-
-        //private string asistent { get; set; }
-
-        public decimal Value { get; set; }
+        public List<Product> Products { get; set; }
 
         public override decimal ValueToPay()
         {
-
+            decimal payroll = 0;
             foreach (Product product in Products)
             {
-                float desc = 0;
-                decimal assi;
-                assi = product.ValueToPay();
-                desc = (float)assi * Discount;
-                Value += assi - (decimal)desc;
-            };
-            return Value;
+                payroll += product.ValueToPay();
+            };            
+            return payroll - (payroll * (decimal)Discount);
         }
         public override string ToString()
         {
-            /*Console.WriteLine(product + "hi");  
-            decimal payroll = 0;
-            foreach (Product product in Products) { 
-                payroll += product.ValueToPay();
-                Console.WriteLine(product+"hi");
-                
-            }*/
+            List<string> products = new List<string>();
 
-            foreach (Product productDiscount in Products)
+            foreach (Product product in Products) 
             {
-                asistent += productDiscount.Description + ",";
+                products.Add(product.Description + ",");
             }
 
             return $"   {Id} {Description}" +
-                $"\n\tProducts...:    {$"{asistent}",9}" +
+                $"\n\tProducts...:    {$"{string.Join(" ", products.ToArray())}",9}" +
                 $"\n\tDiscount...:    {$"{Discount:P2}",13}" +
                 $"\n\tValue......:    {$"{ValueToPay():C2}",9}";
         }
--- Supermarket/Invoice.cs
+++ Supermarket/Invoice.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace Supermarket
 {
-    public class Invoice
+    public class Invoice : IPay
     {
+        private List<Product> _products = new List<Product>();
+        public Invoice() { }
+        public void AddProduct(Product products) { _products.Add(products); }
+        public decimal ValueToPay()
+        {
+            decimal totalProducts = 0;
+            foreach (Product product in _products)
+            {
+                totalProducts += product.ValueToPay();
+            }
+            return totalProducts;
+        }
+        public override string ToString()
+        {
+
+            foreach (Product product in _products)
+            { 
+                Console.WriteLine(product.ToString());
+
+            }
+
+            return $"                     =============="
+                +$"\n\tTOTAL: {$"{ValueToPay():C2}",20}";    
+        }
     }
 }
